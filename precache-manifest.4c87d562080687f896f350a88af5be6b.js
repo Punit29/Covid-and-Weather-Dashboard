961ad672574718a00fe7ca7b2d83e2de4945d633
Punit29 <44257688+Punit29@users.noreply.github.com>
2020-08-14 10:34:56 +0530

    Updates

---
+++ precache-manifest.4c87d562080687f896f350a88af5be6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae0e449efbb47be93cdbecb930205db",
+    "url": "/Covid-and-Weather-Dashboard/index.html"
+  },
+  {
+    "revision": "a8d598ee02de97b7404c",
+    "url": "/Covid-and-Weather-Dashboard/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "d0599449cf202a4b0013",
+    "url": "/Covid-and-Weather-Dashboard/static/css/main.8d2dc1b0.chunk.css"
+  },
+  {
+    "revision": "a8d598ee02de97b7404c",
+    "url": "/Covid-and-Weather-Dashboard/static/js/2.079818eb.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/Covid-and-Weather-Dashboard/static/js/2.079818eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0599449cf202a4b0013",
+    "url": "/Covid-and-Weather-Dashboard/static/js/main.13d89f70.chunk.js"
+  },
+  {
+    "revision": "a0eff7f33f22ac14d970",
+    "url": "/Covid-and-Weather-Dashboard/static/js/runtime-main.124230bf.js"
+  }
+]);
